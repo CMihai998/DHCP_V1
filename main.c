0c0376a47b7a2770e77064d2f9e4840e37e4e399
cMihai998 <mihai3198@gmail.com>
2021-04-10 22:28:17 +0300

    Documented DLL implementation

--- main.c
+++ main.c
@@ -5,26 +5,45 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-struct set_node{
+/*
+ * Node used in DLL
+ *  - data - *in_addr
+ *  - next - *node
+ */
+struct node{
     struct in_addr *data;
-    struct set_node *next;
+    struct node *next;
 };
 
+/*
+ * DLL structure
+ *  - head - *node - first element in DLL
+ *  - tail - *node - last element in DLL
+ */
 struct DLL {
-    struct set_node *head;
-    struct set_node *tail;
+    struct node *head;
+    struct node *tail;
 };
 
+/*
+ * Initializes DLL
+ * @param: list - *DLL: address of list that needs initialization
+ * @return: -
+ */
 void init_DLL(struct DLL *list) {
     list->head = NULL;
     list->tail = NULL;
 }
 
-struct set_node* create_node(struct in_addr* data) {
-    struct set_node *new_node;
+/*
+ * Creates a node that wraps in_addr* data given as input
+ * @param: data - *in_addr: data that needs to be wrapped inside a node
+ * @return: *node which contains data given as @param
+ */
+struct node* create_node(struct in_addr* data) {
+    struct node *new_node;
 
-    new_node = (struct set_node*) malloc(sizeof (struct set_node));
+    new_node = (struct node*) malloc(sizeof (struct node));
 
     new_node->data = data;
     new_node->next = NULL;
@@ -32,8 +51,14 @@ struct set_node* create_node(struct in_addr* data) {
     return new_node;
 }
 
+/*
+ * Creates a node with input data and adds it to the end of the DLL
+ * @param: list - *DLL: list to which we add
+ * @param: data - in_addr*: data that needs to be added to the DLL
+ * @return: -
+ */
 void add_at_tail(struct DLL *list, struct in_addr *data) {
-    struct set_node* new_node = create_node(data);
+    struct node* new_node = create_node(data);
 
     if (list->head == NULL) {
         list->head = new_node;
@@ -44,8 +69,14 @@ void add_at_tail(struct DLL *list, struct in_addr *data) {
     }
 }
 
+/*
+ * Creates a node with input data and adds it to the beggining of the DLL
+ * @param: list - *DLL: list to which we add
+ * @param: data - in_addr*: data that needs to be added to the DLL
+ * @return: -
+ */
 void add_at_head(struct DLL *list, struct in_addr *data) {
-    struct set_node* new_node = create_node(data);
+    struct node* new_node = create_node(data);
 
     if (list->head == NULL) {
         list->head = new_node;
@@ -56,9 +87,13 @@ void add_at_head(struct DLL *list, struct in_addr *data) {
     }
 }
 
-
+/*
+ * Deletes last element of the DLL
+ * @param: list - *DLL: list from which we need to delete, also deallocates the memory occupied by the node
+ * @return: -
+ */
 in_addr_t delete_from_tail(struct DLL *list) {
-    struct set_node *aux;
+    struct node *aux;
     in_addr_t data = -1;
 
     if (list->tail != NULL) {
@@ -77,11 +112,16 @@ in_addr_t delete_from_tail(struct DLL *list) {
     return data;
 }
 
+/*
+ * Deletes first element of the DLL
+ * @param: list - *DLL: list from which we need to delete, also deallocates the memory occupied by the node
+ * @return: -
+ */
 in_addr_t delete_from_head(struct DLL *list) {
     in_addr_t data = -1;
 
     if(list->head != NULL) {
-        struct set_node *to_delete = list->head;
+        struct node *to_delete = list->head;
         data = list->head->data->s_addr;
         list->head = list->head->next;
 
@@ -92,9 +132,14 @@ in_addr_t delete_from_head(struct DLL *list) {
     return data;
 }
 
+/*
+ * Prints all elements of DLL
+ * @param: list - *DLL: list that needs to be printed
+ * @return: -
+ */
 void print_list(struct DLL *list) {
     if (list->head != NULL) {
-        struct set_node *aux;
+        struct node *aux;
 
         aux = list->head;
 
@@ -108,12 +153,18 @@ void print_list(struct DLL *list) {
     }
 }
 
-void empty_lsit(struct DLL *list) {
+/*
+ * Deletes all elements of the list and deallocates memory of list
+ * @param: list - *DLL: list that needs to be deleted
+ * @return: -
+ */
+void empty_list(struct DLL *list) {
     while (list->head != NULL && list->tail != NULL)
         delete_from_head(list);
 
     free(list);
 }
+
 void error(char *message) {
     perror(message);
     exit(0);
@@ -122,7 +173,7 @@ void error(char *message) {
 int handle_listen(int sock, struct sockaddr_in from, int request, int status, int from_length) {
     status = recvfrom(sock, &request, sizeof(int),0, (struct sockaddr *)&from, &from_length);
     if (status < 0) {
-        error("recvfrom()");
+        error("recvfrom() - request failed");
     }
 
     switch (request) {
@@ -146,43 +197,8 @@ int handle_listen(int sock, struct sockaddr_in from, int request, int status, in
     return request;
 }
 
-int main(int argc, char *argv[]) {
-//
-//    int sock, server_length, from_length, n, message_code = 1, request, status;
-//    struct sockaddr_in server;
-//    struct sockaddr_in from;
-//    char buf[1024];
-//
-//
-//    sock = socket(AF_INET, SOCK_DGRAM, 0);
-//    if (sock < 0){
-//        error("socket()");
-//    }
-//
-//    server_length = sizeof server;
-//    bzero(&server, server_length);
-//    server.sin_family = AF_INET;
-//    server.sin_addr.s_addr = INADDR_ANY;
-//    server.sin_port = htons(atoi("8888"));
-//    if (bind(sock, (struct sockaddr*)&server, server_length) < 0) {
-//        error("bind()");
-//    }
-//
-//    from_length = sizeof from;
-//    while(message_code) {
-//        message_code = handle_listen(sock, from, request, status, from_length);
-//    }
-//    set_node* start;
-//    allocate_set_node(start);
-//    set_node *one;
-//    allocate_set_node(one);
-//    set_node *two;
-//    allocate_set_node(two);
-//    set_node *three;
-//    allocate_set_node(three);
-//    set_node *last;
-//    allocate_set_node(last);
-//
+
+void DLL_usage() {
     struct in_addr *d1 = (struct in_addr*) malloc(sizeof (struct in_addr));
     struct in_addr *d2 = (struct in_addr*) malloc(sizeof (struct in_addr));
     struct in_addr *d3 = (struct in_addr*) malloc(sizeof (struct in_addr));
@@ -205,7 +221,37 @@ int main(int argc, char *argv[]) {
     delete_from_tail(list);
     print_list(list);
 
-    empty_lsit(list);
+    empty_list(list);
+}
+
+
+int main(int argc, char *argv[]) {
+
+    int sock, server_length, from_length, n, message_code = 1, request, status;
+    struct sockaddr_in server;
+    struct sockaddr_in from;
+    char buf[1024];
+
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0){
+        error("socket()");
+    }
+
+    server_length = sizeof server;
+    bzero(&server, server_length);
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(atoi("8888"));
+    if (bind(sock, (struct sockaddr*)&server, server_length) < 0) {
+        error("bind()");
+    }
+
+    from_length = sizeof from;
+    while(message_code) {
+        message_code = handle_listen(sock, from, request, status, from_length);
+    }
+
     return 0;
 }
 
